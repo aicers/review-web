36b8921af3c3d6d50c91c1d4c7b61dbba0345be3
David <104803357+kimhanbeom@users.noreply.github.com>
2025-06-14 08:34:21 +0900

    Fix Clippy warnings caused by rustc 1.87.0
    Close: #456

--- src/graphql.rs
+++ src/graphql.rs
@@ -724,7 +724,7 @@ impl TestSchema {
     async fn execute_stream(
         &self,
         subscription: &str,
-    ) -> impl futures_util::Stream<Item = async_graphql::Response> + use<> {
+    ) -> impl futures_util::Stream<Item = async_graphql::Response> + use<'_> {
         let request: async_graphql::Request = subscription.into();
         self.schema
             .execute_stream(request.data(RoleGuard::Role(Role::SystemAdministrator)))
